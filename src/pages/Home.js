a5e074309d8b7c00c2490a03ead8b535281c5590
mau <mauriciorj76@gmail.com>
2021-05-14 08:25:46 -0600

    antes del css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
-const Home = () => {
+import getData from "../utils/getData";
+
+const Home = async () => {
+    const characters = await getData();
     const view = `
     <div class="Characters">
+
+        ${characters.results.map(characters => `
         <article class="Character-item">
-            <a href="#/1/">
-                <img src="image" alt="name">
-                <h2>name</h2>
+            <a href="#/${characters.id}/">
+                <img src="${characters.image}" alt="${characters.name}">
+                <h2>${characters.name}</h2>
             </a>
-        </article>
+        </article>  
+        `).join('')}
+
     </div>
     `;
     return view;
